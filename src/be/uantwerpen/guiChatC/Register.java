10e764da9174752c9f94c05e3a34cb7593696bb7
Michiel <michielaernouts@hotmail.com>
2015-11-03 12:17:03 +0100

    toevoegen GUI van Login en Register forms

--- src/be/uantwerpen/guiChatC/Register.java
+++ src/be/uantwerpen/guiChatC/Register.java
@@ -6,14 +6,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.Naming;
+import java.util.Arrays;
 
 /**
  * Created by Michiel on 30/10/2015.
  */
 public class Register extends JFrame {
     private JTextField txtFullName;
-    private JTextField txtPw;
-    private JTextField txtConfirmPw;
     private JButton btnRegister;
     private JTextField txtUserName;
     private JLabel lblFullName;
@@ -21,6 +20,8 @@ public class Register extends JFrame {
     private JLabel lblPw;
     private JLabel lblConfirmPW;
     private JPanel pnlRegister;
+    private JPasswordField pwfPw;
+    private JPasswordField pwfConfirmPw;
 
     public Register() {
         super("Nieuwe gebruiker registreren");
@@ -30,6 +31,8 @@ public class Register extends JFrame {
 
         addListeners();
 
+        txtFullName.setText("Test User");
+        txtUserName.setText("test@1337.com");
     }
 
     private void addListeners() {
@@ -39,18 +42,25 @@ public class Register extends JFrame {
                 //gegevens lezen
                 String fullName = txtFullName.getText();
                 String user = txtUserName.getText();
-                String pw = txtPw.getText();
-                String confirm = txtConfirmPw.getText();
 
-                if (user.equals("") | pw.equals("")| fullName.equals("")| confirm.equals(""))  {
+                //password lezen en hashen
+                String pwInput = (String.valueOf(pwfPw.getPassword()));
+                String confirmInput = (String.valueOf(pwfConfirmPw.getPassword()));
+                String pwOutput = pwInput.hashCode()+"";
+
+                //Kijken of de velden juist ingevuld zijn
+                if (user.equals("") | pwInput.equals("")| fullName.equals("")| confirmInput.equals(""))  {
                     JOptionPane.showMessageDialog(null,"Voer alle gegevens juist in!");
                 }
-                else if(pw.equals(confirm)){
+                else if(Arrays.equals(pwfPw.getPassword(), pwfConfirmPw.getPassword() )){
                     //registreren
                     try{
                         IChatServer cs = (IChatServer) Naming.lookup("//" + "127.0.0.1:11337" + "/ChatServer");
                         //IChatServer cs = (IChatServer) Naming.lookup("//" + "localhost" + "/Chatserver");
-                        cs.register(user, pw, fullName);
+                        cs.register(user, pwOutput, fullName);
+
+                        HomePage homepageForm = new HomePage();
+                        dispose();
                     }
                     catch(Exception x){
                         System.out.println("Client exception" + x.getMessage());
@@ -59,6 +69,7 @@ public class Register extends JFrame {
                 }
                 else{
                     JOptionPane.showMessageDialog(null,"De wachtwoorden komen niet overeen!");
+                    System.out.println(pwInput + " " + pwOutput);
             }
         }
     });
